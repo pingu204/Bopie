196f4802b41e7e55e3270017f8fb2b1625d75f4a
pingu204 <73891290+pingu204@users.noreply.github.com>
2024-08-25 23:09:03 +0800

    Added JS
    Automated card creation; add new items in data.js

---
+++ js/data.js
@@ -0,0 +1,136 @@
+var cards = [
+    {
+        "title" : "'New Woman'-Inspired Shoot",
+        "new" : true,
+        "description" : "Regina Benitez showcases her final (white) and preliminary (black) evening gowns for the Miss Universe 2024 competition. The shoot takes inspiration from the music video of LISA's 'New Woman'.",
+        "date" : "AUGUST 2024",
+        "cover" : "./images/2024/Regina_NewWoman/new woman.png",
+        "modalID" : "reginaNewWoman",
+        "images" : [
+            "./images/2024/Regina_NewWoman/new woman.png",
+            "./images/2024/Regina_NewWoman/new woman_1.png",
+            "./images/2024/Regina_NewWoman/new woman_2.png",
+        ],
+        "url" : null
+    },
+    {
+        "title" : "Miss Sim Universe 2024 Swimsuit Competition",
+        "new" : false,
+        "description" : "The eight semi-finalists of the Miss Sim Universe 2024 competition strut their way to the Swimsuit Round, showcasing the bedazzled swimsuit designed by MSSIMS and heels sponsored by Astya.",
+        "date" : "AUGUST 2024",
+        "cover" : "./images/2024/MSU_Swimsuit/swim-cover.jpg",
+        "modalID" : "msuSwimsuit",
+        "images" : [],
+        "url" : "https://youtu.be/Rqi32rsisIA"
+    },
+    {
+        "title" : "'Birds Of A Feather' Titlecard",
+        "new" : false,
+        "description" : "Billie Eilish's hit song 'Birds Of A Feather' lays an interesting perspective on falling in love that touches on the topic of death. This set of title cards aim to encapsulate that cathartic feeling, playing with the imagery of coffins overlayed on two partners who seem to be having the time of their lives together.",
+        "date" : "JULY 2024",
+        "cover" : "./images/2024/BirdsOfAFeather/boaf.png",
+        "modalID" : "boaf",
+        "images" : [
+            "./images/2024/BirdsOfAFeather/boaf.png",
+            "./images/2024/BirdsOfAFeather/boaf_2.png"
+        ],
+        "url" : null
+    },
+    {
+        "title" : "Regina Benitez for 'BRAGAIS'",
+        "new" : false,
+        "description" : "With the theme, 'Saccharine Elegance', Regina heels is the amalgamation of luscious glitter straps and an open toe box that gives the female feet a silhouette of elegance, while maintaining support and comfort in line with the principles of the brand.",
+        "date" : "MAY 2024",
+        "cover" : "./images/2024/Regina_Bragais/bragais_regina.png",
+        "modalID" : "reginaBragais",
+        "images" : [
+            "./images/2024/Regina_Bragais/bragais_regina.png",
+            "./images/2024/Regina_Bragais/bragais_regina_2.png"
+        ],
+        "url" : null
+    },
+    {
+        "title" : "Theo Song for GQ's April 2024 Issue",
+        "new" : false,
+        "description" : "Theo Song graces the cover of GQ, embodying a dynamic blend of masculinity and style. With his confident presence and sleek wardrobe choices, Theo exudes both strength and elegance, capturing the essence of modern masculinity. The cover celebrates his athletic prowess and explores themes of self-assurance, versatility, and the intersection of sports and high fashion, making a powerful statement on contemporary male identity.",
+        "date" : "APRIL 2024",
+        "cover" : "./images/2024/Theo_GQ/gq.png",
+        "modalID" : "theoGQ",
+        "images" : [
+            "./images/2024/Theo_GQ/gq.png",
+            "./images/2024/Theo_GQ/gq_1.png"
+        ],
+        "url" : null
+    },
+    {
+        "title" : "Maia Cazares for 'Midas Touch'",
+        "new" : false,
+        "description" : "Maia Cazares becomes the face of Midas Touch, a luxury swimwear brand renowned for its golden allure. Embodying the brand's ethos of transformation, Maia effortlessly personifies the idea of turning everything she touches into gold. With her radiant confidence and timeless elegance, she elevates the brand's image, making each piece she wears a symbol of opulence and allure.",
+        "date" : "APRIL 2024",
+        "cover" : "./images/2024/Maia_MidasTouch/midas cover.png",
+        "modalID" : "maiaMidas",
+        "images" : [
+            "./images/2024/Maia_MidasTouch/midas cover.png",
+            "./images/2024/Maia_MidasTouch/midas-1.png",
+            "./images/2024/Maia_MidasTouch/midas-2.png",
+            "./images/2024/Maia_MidasTouch/midas-3.png"
+        ],
+        "url" : null
+    },
+    {
+        "title" : "Dahlia Sisson for 'MUGLER'",
+        "new" : false,
+        "description" : "Step into the world of high fashion with the latest Mugler photoshoot featuring the stunning Dahlia Sisson. Embodying the brand's signature boldness and futuristic flair, Dahlia captivates with her powerful presence, showcasing the intricate designs and avant-garde aesthetics that define Mugler.",
+        "date" : "JANUARY 2024",
+        "cover" : "./images/2024/Dahlia_Mugler/mugler.png",
+        "modalID" : "dahliaMugler",
+        "images" : [
+            "./images/2024/Dahlia_Mugler/mugler.png",
+            "./images/2024/Dahlia_Mugler/mugler-1.png",
+            "./images/2024/Dahlia_Mugler/mugler-2.png",
+            "./images/2024/Dahlia_Mugler/mugler-3.png",
+        ],
+        "url" : null
+    },
+    {
+        "title" : "'Regina Benitez' Lookbook for Miss Sim Universe 2024",
+        "new" : false,
+        "description" : "Regina Benitez’s customized lookbook for Miss Sim Universe 2024 is a stunning showcase of her journey to the crown. Each page highlights her unique style, from elegant evening gowns to bold, contemporary fashion, reflecting her vibrant personality and impeccable taste. The lookbook captures Regina’s grace, confidence, and readiness to take on the world stage, making it a must-see for fans and fashion enthusiasts alike.",
+        "date" : "",
+        "cover" : "./images/2024/Regina_Lookbook/arrival.png",
+        "modalID" : "reginaLookbook",
+        "images" : [
+            "./images/2024/Regina_Lookbook/arrival.png",
+            "./images/2024/Regina_Lookbook/L1.png",
+            "./images/2024/Regina_Lookbook/L2.png",
+            "./images/2024/Regina_Lookbook/L3.png",
+            "./images/2024/Regina_Lookbook/L4.png",
+            "./images/2024/Regina_Lookbook/L5.png",
+            "./images/2024/Regina_Lookbook/L6.png",
+            "./images/2024/Regina_Lookbook/L7-2.png",
+            "./images/2024/Regina_Lookbook/L8.png",
+            "./images/2024/Regina_Lookbook/L9.png",
+            "./images/2024/Regina_Lookbook/L10.png",
+            "./images/2024/Regina_Lookbook/L11.png",
+            "./images/2024/Regina_Lookbook/L12.png"
+        ],
+        "url" : null
+    },
+    {
+        "title" : "Maia Cazares for 'Fairy Skin' and 'MEGA' Magazine",
+        "new" : false,
+        "description" : "Maia Cazares shines as the new brand ambassador for Fairy Skin, embodying the brand's commitment to radiant beauty and effortless elegance. Her natural charm and glowing complexion make her the perfect fit for Fairy Skin’s message of self-love and skincare excellence. Featured in MEGA magazine, Maia's collaboration with the brand is celebrated in a stunning editorial that highlights her beauty, poise, and the transformative power of a flawless skincare routine.",
+        "date" : "SEPTEMBER 2023",
+        "cover" : "./images/2023/Maia_FairySkin/5.png",
+        "modalID" : "maiaFairySkin",
+        "images" : [
+            "./images/2023/Maia_FairySkin/1.png",
+            "./images/2023/Maia_FairySkin/2.png",
+            "./images/2023/Maia_FairySkin/3.png",
+            "./images/2023/Maia_FairySkin/4.png",
+            "./images/2023/Maia_FairySkin/5.png",
+            "./images/2023/Maia_FairySkin/mega_1.png",
+            "./images/2023/Maia_FairySkin/mega_2.png"
+        ]
+    }
+]
